5854d48cf7eac6447eb4def6f91563113609cc0c
Francisco Castellanos <fvcastellanos@gmail.com>
2018-10-13 11:51:48 -0600

    initial commit

---
+++ src/main/java/edu/umg/farm/cucumber/farmcontroltest/FarmControlTestApplication.java
@@ -0,0 +1,12 @@
+package edu.umg.farm.cucumber.farmcontroltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmControlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmControlTestApplication.class, args);
+	}
+}
